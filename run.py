437f65fda08eff1ed35b601985f1e164739959f8
Ryagg <58631455+Ryagg@users.noreply.github.com>
2021-09-04 20:27:47 +0200

    feat: print stock recommendations for the user

--- run.py
+++ run.py
@@ -95,6 +95,7 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    get_stock_values(stock_data)
 
 
 def get_last_5_entries_sales():
@@ -127,6 +128,15 @@ def calculate_stock_data(data):
         new_stock_data.append(round(stock_num))
 
     return new_stock_data
+
+
+def get_stock_values(data):
+    headings = SHEET.worksheet('stock').row_values(1)
+    print(headings)
+    print(data)
+    output = dict(zip(headings, data))
+    print(output)
+
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
